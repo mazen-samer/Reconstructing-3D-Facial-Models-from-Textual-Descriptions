9ca2f1dd9fccd369e3877b233465857b4486c3f2
Mazen Samer Abdallah <mazen.samer.2000@gmail.com>
2024-04-23 21:45:01 +0200

    setup and 3D adjustments

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import SignIn from "./pages/SignIn";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<>home</>} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/ModelCanvas.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Stage, PresentationControls, OrbitControls } from "@react-three/drei";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { Box3, Vector3 } from "three";
+import { baseUrl } from "../constants/baseUrl";
+
+function Model({ obj }) {
+  return obj ? <primitive object={obj} scale={0.007} /> : null;
+}
+
+export default function ModelCanvas() {
+  const [obj, setObj] = useState(null);
+  const [cameraPosition, setCameraPosition] = useState([0, 0, 500]);
+  const canvasSize = [200, 200];
+  useEffect(() => {
+    const fetchObj = async () => {
+      try {
+        const response = await fetch(`${baseUrl}/static/3dobjs/1`);
+        const objText = await response.text();
+        const loader = new OBJLoader();
+        const loadedObj = loader.parse(objText);
+        const boundingBox = new Box3().setFromObject(loadedObj);
+        const center = boundingBox.getCenter(new Vector3());
+
+        loadedObj.position.sub(center);
+
+        setObj(loadedObj);
+      } catch (error) {
+        console.error("Error loading OBJ file:", error);
+      }
+    };
+
+    fetchObj();
+  }, []);
+
+  useEffect(() => {
+    if (obj) {
+      const boundingBox = new Box3().setFromObject(obj);
+      const center = boundingBox.getCenter(new Vector3());
+      const distance = Math.max(
+        boundingBox.max.x - boundingBox.min.x,
+        boundingBox.max.y - boundingBox.min.y,
+        boundingBox.max.z - boundingBox.min.z
+      );
+
+      const newCameraPosition = [center.x, center.y, 150 + distance];
+
+      setCameraPosition(newCameraPosition);
+    }
+  }, [obj]);
+
+  return (
+    <Canvas
+      dpr={[1, 2]}
+      camera={{ fov: 45, position: cameraPosition }}
+      style={{
+        width: canvasSize[0],
+        height: canvasSize[1],
+      }}
+    >
+      <directionalLight position={[5, 5, 20]} intensity={0.8} />
+      <PresentationControls speed={1}>
+        <Stage environment={"studio"}>
+          <Model obj={obj} />
+        </Stage>
+        <OrbitControls enableZoom />
+      </PresentationControls>
+    </Canvas>
+  );
+}
