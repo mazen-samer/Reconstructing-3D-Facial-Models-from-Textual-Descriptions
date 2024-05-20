a79970470fb7d02cfc26150e61fcc14a5e66c975
Mazen Samer Abdallah <mazen.samer.2000@gmail.com>
2024-05-20 21:25:56 +0300

    [Theme] assets + theme change

--- client/src/constants/theme.js
+++ client/src/constants/theme.js
@@ -1,5 +1,12 @@
 export const themeData = {
   token: {
     fontFamily: "Montserrat",
+    colorTextBase: "#f5f5f5",
+  },
+  components: {
+    Input: {
+      colorBorder: "grey",
+      colorBgContainer: "#111111",
+    },
   },
 };
